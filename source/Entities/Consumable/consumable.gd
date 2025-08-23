94d602d4f57495451add3e9cca143c43432ce09e
KaelPearson <kaelpearson@gmail.com>
2025-08-23 15:57:52 -0700

    consumable and amy angler

--- source/Entities/Consumable/consumable.gd
+++ source/Entities/Consumable/consumable.gd
@@ -4,7 +4,13 @@ extends Area2D
 @export var heat_amt : float = 0.0;
 @export var air_amt : float = 0.0;
 
+func _ready() -> void:
+	area_entered.connect(_area_entered)
 
-
-func area_entered(_area: Area2D) -> void :
-	print("entered");
+func _area_entered(_area: Area2D) -> void :
+	var bodies = get_overlapping_bodies();
+	for body in bodies :
+		if (body is Player) :
+			body.add_air(air_amt);
+			body.add_heat(heat_amt);
+			print("add coin too TODO");
