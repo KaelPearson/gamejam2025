0cf0eaea0a86e82f178b4a381a574b0e4df1e1a0
Adam Toth <adamjmtoth@gmail.com>
2025-08-24 08:40:46 -0700

    game UI touch ups

--- source/Components/GameUi/game_ui.gd
+++ source/Components/GameUi/game_ui.gd
@@ -4,15 +4,17 @@ extends Control
 @onready var heat: ProgressBar = $CanvasLayer/Heat
 @onready var air: ProgressBar = $CanvasLayer/Air
 
-@onready var life_1: TextureRect = $CanvasLayer/MarginContainer/GridContainer2/life1
-@onready var life_2: TextureRect = $CanvasLayer/MarginContainer/GridContainer2/life2
-@onready var life_3: TextureRect = $CanvasLayer/MarginContainer/GridContainer2/life3
+@onready var life_pool: Array[TextureRect] = [
+	$CanvasLayer/MarginContainer/GridContainer2/life1,
+	$CanvasLayer/MarginContainer/GridContainer2/life2,
+	$CanvasLayer/MarginContainer/GridContainer2/life3,
+]
 
+var head_hurt = preload("res://Assets/dog/Dog_Head_Hit.webp")
 var score_value: float = 0.0
 var score_running: bool = false
-var heat_value: float = 0.0    
-var air_value: float = 100.0 
-var max_life: float = 3
+var max_health: int = 3
+var current_health: int = max_health
 
 @export var heat_increase_rate: float = 1.0 
 @export var air_decrease_rate: float = 1.0
@@ -22,35 +24,14 @@ func _ready() -> void:
 	# Hide all children at the start
 	for child in get_children():
 		child.visible = false
-	heat.value = heat_value 
-	air.value = air_value
-
-func _process(delta: float) -> void:
-	#if score_running:
-		#score_value += 10 * delta
-		#score_label.text = str(int(score_value))
-		
-	#for heat
-	heat_value += heat_increase_rate * delta
-	heat_value = min(heat_value, 100)  
-	heat.value = heat_value
-	
-	#for air
-	air_value -= air_decrease_rate * delta
-	air_value = max(air_value, 0)
-	air.value = air_value
+	heat.value = 0.0
+	air.value = 100.0
 
 func start_score() -> void:
 	for child in get_children():
 		child.visible = true
-	score_running = true
-	score_value = 0 
-	  
-	heat_value = 0
-	heat.value = heat_value
 	
-	air_value = 100
-	air_value = air_value
+	score_running = true
 
 func stop_score() -> void:
 	score_running = false
@@ -61,15 +42,27 @@ func _on_player_set():
 	Globals.player.score_update.connect(_on_score_change)
 	Globals.player.heat_update.connect(_on_heat_change)
 	Globals.player.air_update.connect(_on_air_change)
-	
+	Globals.player.health_component.health_changed.connect(_on_health_changed)
+	Globals.player.health_component.death.connect(stop_score)
+	max_health = Globals.player.health_component.max_health
+	current_health = Globals.player.health_component.current_health
+
+
 func _on_heat_change(new_heat):
-	heat_value = clamp(new_heat, 0, 100)
-	heat.value = heat_value
-	
+	if score_running:
+		heat.value = clampf(new_heat, 0, 100)
+
 func _on_air_change(new_air):
-	air_value = clamp(new_air, 0, 100)
-	air.value = air_value
+	if score_running:
+		air.value = clampf(new_air, 0, 100)
 
 func _on_score_change(new_score):
 	if score_running:
+		score_value = new_score
 		score_label.text = str(int(new_score))
+
+func _on_health_changed(amount):
+	current_health += amount
+	for health in range(max_health):
+		if health >= current_health:
+			life_pool[health].texture = head_hurt
