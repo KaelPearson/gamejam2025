ba478607d83c8574545e8f2339e1776f56472434
Jacob Tizel <jacob.tizel@gmail.com>
2025-08-24 13:14:56 -0700

    angler meow

--- source/Entities/Enemy/angler.gd
+++ source/Entities/Enemy/angler.gd
@@ -13,6 +13,7 @@ func _on_area_entered(body) -> void:
 	if body.get_parent() is Player:
 		um.visible = true;
 		reg.visible = false;
+		$EvilMeow.play()
 		var tween = create_tween()
 		tween.tween_property($EnemyMovementComponent, "speed", $EnemyMovementComponent.speed * 5, 1)
 		$PlayerDetector.queue_free()
