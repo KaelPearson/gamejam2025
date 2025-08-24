f784e5942c758fd3618b577e379c29de0e0c3a57
KaelPearson <kaelpearson@gmail.com>
2025-08-23 18:52:53 -0700

    i_frames and death signal

--- source/Components/HealthComponent/health_component.gd
+++ source/Components/HealthComponent/health_component.gd
@@ -8,12 +8,32 @@ var current_health := max_health
 ## Negative numbers are damage, positive numbers are healing.
 signal health_changed(amount)
 
+## Emits when player died
+signal death();
+
+@onready var i_frame_timer = Timer.new();
+var i_frames = false;
+
+func _ready() -> void:
+	add_child(i_frame_timer);
+	i_frame_timer.wait_time = 1.0;
+	i_frame_timer.one_shot = true;
+	i_frame_timer.timeout.connect(_can_be_hit);
+
+func _can_be_hit() -> void :
+	i_frames = false;
+
 func heal(amount: int) -> void:
 	change_health(amount)
 
 func damage(amount: int) -> void:
+	if (i_frames) : return;
 	change_health(-amount)
+	i_frame_timer.start();
+	i_frames = true;
 
 func change_health(amount: int) -> void:
 	current_health = clampi(current_health + amount, 0, max_health)
 	emit_signal("health_changed", amount)
+	if (current_health <= 0) :
+		death.emit();
