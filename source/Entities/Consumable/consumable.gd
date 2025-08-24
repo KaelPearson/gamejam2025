80541b097df243066d5b6813e4fb161a841e1a84
Jacob Tizel <jacob.tizel@gmail.com>
2025-08-24 14:19:22 -0700

    fix sfx not working underwater

--- source/Entities/Consumable/consumable.gd
+++ source/Entities/Consumable/consumable.gd
@@ -12,20 +12,23 @@ class_name Consumable
 func _ready() -> void:
 	area_entered.connect(_area_entered)
 
-func play_one_shot(stream: AudioStream, position: Vector2 = Vector2.ZERO):
-	var player = AudioStreamPlayer2D.new()  # or AudioStreamPlayer2D for 2D games
-	player.stream = stream
-	player.transform.origin = position
-	get_tree().current_scene.add_child(player)
-	player.play()
-	player.connect("finished", Callable(player, "queue_free"), CONNECT_ONE_SHOT)
+#func play_one_shot(stream: AudioStream, position: Vector2 = Vector2.ZERO):
+	#var player = AudioStreamPlayer2D.new()  # or AudioStreamPlayer2D for 2D games
+	#player.stream = stream
+	#player.transform.origin = position
+	#get_tree().current_scene.add_child(player)
+	#player.play()
+	#player.connect("finished", Callable(player, "queue_free"), CONNECT_ONE_SHOT)
 
 func _area_entered(_area: Area2D) -> void :
 	var bodies = get_overlapping_areas();
 	for body in bodies :
 		if (body is HurtboxComponent) :
 			body.collect_to_player(heat_amt, air_amt, coin_amt, life_amt);
-			play_one_shot($PickupSFX.stream, $PickupSFX.position)
+			#play_one_shot($PickupSFX.stream, $PickupSFX.position)
+			$Sprite.hide()
+			$PickupSFX.play()
+			await pickup_sfx.finished
 			queue_free();
 			
 			
