c00a99e8e0aaad9a77e932573e44cf7782704a28
Mikee <hiercomichael@gmail.com>
2025-08-24 00:08:32 -0700

    fixed signals for UI

--- source/Components/GameUi/game_ui.gd
+++ source/Components/GameUi/game_ui.gd
@@ -4,11 +4,15 @@ extends Control
 @onready var heat: ProgressBar = $CanvasLayer/Heat
 @onready var air: ProgressBar = $CanvasLayer/Air
 
+@onready var life_1: TextureRect = $CanvasLayer/MarginContainer/GridContainer2/life1
+@onready var life_2: TextureRect = $CanvasLayer/MarginContainer/GridContainer2/life2
+@onready var life_3: TextureRect = $CanvasLayer/MarginContainer/GridContainer2/life3
 
 var score_value: float = 0.0
 var score_running: bool = false
 var heat_value: float = 0.0    
-var air_value: float = 100.0  
+var air_value: float = 100.0 
+var max_life: float = 3
 
 @export var heat_increase_rate: float = 1.0 
 @export var air_decrease_rate: float = 1.0
@@ -55,6 +59,16 @@ func stop_score() -> void:
 ## Player signals cannot be set in the ready function as a player may not be set when the UI is first ready.
 func _on_player_set():
 	Globals.player.score_update.connect(_on_score_change)
+	Globals.player.heat_update.connect(_on_heat_change)
+	Globals.player.air_update.connect(_on_air_change)
+	
+func _on_heat_change(new_heat):
+	heat_value = clamp(new_heat, 0, 100)
+	heat.value = heat_value
+	
+func _on_air_change(new_air):
+	air_value = clamp(new_air, 0, 100)
+	air.value = air_value
 
 func _on_score_change(new_score):
 	if score_running:
